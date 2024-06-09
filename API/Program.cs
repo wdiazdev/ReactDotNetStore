a015bc04890e3dabf107fa034bd289a6b1782b0c
Wilfredo Diaz <wdiazdev@gmail.com>
2024-06-09 10:52:19 -0400

    Migrations created. db created. Db initializer created.

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,16 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
