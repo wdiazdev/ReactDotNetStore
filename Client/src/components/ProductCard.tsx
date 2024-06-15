ee730f9698e201f54f17f24fdd1e1e0c0d26066a
Wilfredo Diaz <wdiazdev@gmail.com>
2024-06-15 11:04:25 -0400

    Axios added. product by ID API implemented.

--- Client/src/components/ProductCard.tsx
+++ Client/src/components/ProductCard.tsx
@@ -9,6 +9,7 @@ import {
   Typography,
 } from "@mui/material"
 import { Product } from "../models"
+import { Link } from "react-router-dom"
 
 type Props = {
   product: Product
@@ -50,7 +51,9 @@ export default function ProductCard({ product }: Props) {
       </CardContent>
       <CardActions>
         <Button size="small">Add to cart</Button>
-        <Button size="small">View</Button>
+        <Button size="small" component={Link} to={`/catalog/${product.id}`}>
+          View
+        </Button>
       </CardActions>
     </Card>
   )
--- Client/src/pages/ProductDetails.tsx
+++ Client/src/pages/ProductDetails.tsx
@@ -1,5 +1,28 @@
 import { Typography } from "@mui/material"
+import axios from "axios"
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { Product } from "../models"
 
 export default function ProductDetails() {
-  return <Typography variant="h2">Product Details</Typography>
+  const { id } = useParams<{ id: string }>()
+
+  const [product, setProduct] = useState<Product | null>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/Products/${id}`)
+      .then((response) => setProduct(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false))
+  }, [id])
+
+  if (loading) {
+    return <Typography variant="h2">Loading...</Typography>
+  }
+  if (!loading && !product) {
+    return <Typography variant="h2">Product not found</Typography>
+  }
+  return <Typography variant="h2">{product?.name}</Typography>
 }
