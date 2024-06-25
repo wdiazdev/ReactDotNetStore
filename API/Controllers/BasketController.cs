cf4c0e9c78401f2cd483ab2f120686eb197fe84c
Wilfredo Diaz <wdiazdev@gmail.com>
2024-06-24 23:01:25 -0400

    Basket and BasketItems DTO created. Add item API updated. Remove item API created.

--- API/Controllers/BasketController.cs
+++ API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,23 +15,37 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<Basket>> GetBasket()
+        public async Task<ActionResult<BasketDto>> GetBasket()
         {
             var basket = await RetrieveBasket();
-
             if (basket == null) return NotFound();
 
-            return basket;
+            return new BasketDto
+            {
+                Id = basket.Id,
+                BuyerId = basket.BuyerId,
+                Items = basket.Items.Select(item => new BasketItemDto
+                {
+                    ProductId = item.ProductId,
+                    Name = item.Product.Name,
+                    Price = item.Product.Price,
+                    PictureUrl = item.Product.PictureUrl,
+                    Type = item.Product.Type,
+                    Brand = item.Product.Brand,
+                    Quantity = item.Quantity
+                }).ToList()                
+            };
         }
 
         [HttpPost]
         public async Task<ActionResult> AddItemToBasket(int productId, int quantity)
         {
              var basket = await RetrieveBasket();
+            if (basket == null) basket = CreatedBasket();
 
-             if (basket == null) basket = CreatedBasket();
              var product = await _context.Products.FindAsync(productId);
              if (product == null) return NotFound();
+
              basket.AddItem(product, quantity);
 
              var result = await _context.SaveChangesAsync() > 0;
@@ -41,10 +56,19 @@ namespace API.Controllers
         }
 
         [HttpDelete]
-        // public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
-        // {
-        //     return Ok();
-        // }
+        public async Task<ActionResult> RemoveBasketItem(int productId, int quantity)
+        {
+             var basket = await RetrieveBasket();
+             if (basket == null) return NotFound();
+
+             basket.RemoveItem(productId, quantity);
+
+             var result = await _context.SaveChangesAsync() > 0;
+
+             if (result) return Ok();
+
+             return BadRequest(new ProblemDetails{Title= "Problem removing item from basket"});
+        }
 
         private async Task<Basket> RetrieveBasket()
         {
