03c7478a94353a0a4999e6a7c2c23f1dc670cccb
Wilfredo Diaz <wdiazdev@gmail.com>
2024-07-07 14:41:47 -0400

    Search and filter by extensions added to the products controller

--- API/Controllers/ProductsControllers.cs
+++ API/Controllers/ProductsControllers.cs
@@ -17,10 +17,18 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<List<Product>>> GetProducts(string orderBy)
+        public async Task<ActionResult<List<Product>>> GetProducts
+        (
+            string orderBy, 
+            string searchTerm,
+            string brands,
+            string types
+        )
         { 
             var query = _context.Products
                 .Sort(orderBy)
+                .Search(searchTerm)
+                .Filter(brands, types)
                 .AsQueryable();
 
             return await query.ToListAsync();
