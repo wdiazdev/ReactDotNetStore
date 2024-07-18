5a761aa960aff3e992abfc9b42da5a29edd5be67
Wilfredo Diaz <wdiazdev@gmail.com>
2024-07-17 22:40:41 -0400

    Token services added. Account controller updated to return user email and token.

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using API.Data;
 using API.Entities;
 using API.Middleware;
+using API.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -22,6 +23,7 @@ builder.Services.AddIdentityCore<User>(opt =>
     .AddEntityFrameworkStores<StoreContext>();
 builder.Services.AddAuthentication();
 builder.Services.AddAuthorization();
+builder.Services.AddScoped<TokenService>();
 
 var app = builder.Build();
 
