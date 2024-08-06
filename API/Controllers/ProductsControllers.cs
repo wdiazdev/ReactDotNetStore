ce0104a602deec6ff18272e13e104b11a758b4a8
Wilfredo Diaz <wdiazdev@gmail.com>
2024-08-06 13:56:21 -0400

    Ability to update and delete products images implemented.

--- API/Controllers/ProductsControllers.cs
+++ API/Controllers/ProductsControllers.cs
@@ -95,7 +95,7 @@ namespace API.Controllers
 
         [Authorize(Roles = "Admin")]
         [HttpPut]
-        public async Task<ActionResult<Product>> UpdateProduct(UpdateProductDto productDto)
+        public async Task<ActionResult<Product>> UpdateProduct([FromForm]UpdateProductDto productDto)
         {
             var product = await _context.Products.FindAsync(productDto.Id);
 
@@ -103,9 +103,24 @@ namespace API.Controllers
 
             _mapper.Map(productDto, product);
 
+            if (productDto.File != null)
+            {
+                var imageResult = await _imageService.AddImageAsync(productDto.File);
+
+                if (imageResult.Error != null) 
+                    return BadRequest(new ProblemDetails{Title = imageResult.Error.Message});
+
+                if (!string.IsNullOrEmpty(product.PublicId)) 
+                    await _imageService.DeleteImageAsync(product.PublicId);
+                
+                product.PictureUrl = imageResult.SecureUrl.ToString();
+
+                product.PublicId = imageResult.PublicId; 
+            } 
+
             var result = await _context.SaveChangesAsync() > 0;
 
-            if (result) return NoContent();
+            if (result) return Ok(product);
 
             return BadRequest(new ProblemDetails {Title = "Product update failed"});
         }
@@ -117,6 +132,9 @@ namespace API.Controllers
             var product = await _context.Products.FindAsync(id);
 
             if (product == null) return NotFound();
+
+            if (!string.IsNullOrEmpty(product.PublicId)) 
+                    await _imageService.DeleteImageAsync(product.PublicId);
 
             _context.Products.Remove(product);
 
